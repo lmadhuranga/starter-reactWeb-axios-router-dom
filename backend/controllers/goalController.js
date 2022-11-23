a11863116495b2beca18121106e4dbc7aaae1a2e
Madhuranga Senadheera <madhuranga.senadeera@softlogiclife.lk>
2022-11-23 20:26:37 +0400

    fixed model godal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,8 +1,9 @@
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
+const Goal = require('../models/goalModel')
 
 const getGoals =  asyncHandler( async (req, res)=>{
-    const msg = 'get goals'
-    res.status(200).json({msg})
+    const goals = await Goal.find() 
+    res.status(200).json(goals)
 })
 
 const createPost = asyncHandler( async (req, res)=>{ 
@@ -10,20 +11,33 @@ const createPost = asyncHandler( async (req, res)=>{
         res.status(400)
         throw new Error('Please enter name')
     }
-
-    const msg = 'create goals'
-    res.status(200).json({msg})
+    
+    const goal = await Goal.create({
+        name:req.body.name
+    }) 
+    res.status(203).json(goal)
 })
 
 const udpatePost =  asyncHandler( async (req, res)=>{
-    
-    const msg = `update golas ${req.params.id}`
-    res.status(200).json({msg})
+    const goal = await Goal.findById(req.params.id)
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const udpateGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.status(200).json(udpateGoal)
 })
 
 const deletePost =  asyncHandler( async (req, res)=>{
-    const msg = `delete golas ${req.params.id}` 
-    res.status(200).json({msg})
+    const goal = await Goal.findById(req.params.id)
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const udpateGoal = await goal.remove();
+    res.status(200).json({"msg":'Goal deleted'})
 })
 
 
